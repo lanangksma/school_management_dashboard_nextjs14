5bd6a64e2bc32d5ba1f743517c8c2481734088d4
lanangksma <kusumalanang1@gmail.com>
2024-09-16 04:58:40 +0700

    lineChart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,7 @@
 import UserCard from "@/components/UserCard";
 import CountChart from "@/components/CountChart";
 import AttendanceChart from "@/components/AttendanceChart";
+import FinanceChart from "@/components/FinanceChart";
 
 const AdminPage = () => {
     return (
@@ -9,24 +10,25 @@ const AdminPage = () => {
             <div className="flex flex-col gap-8 w-full lg:w-2/3">
                 {/*USER CARDS*/}
                 <div className="flex gap-4 justify-between flex-wrap">
-                    <UserCard type="student" />
-                    <UserCard type="teacher" />
-                    <UserCard type="parent" />
-                    <UserCard type="staff" />
+                    <UserCard type="student"/>
+                    <UserCard type="teacher"/>
+                    <UserCard type="parent"/>
+                    <UserCard type="staff"/>
                 </div>
-                <div>
-                    {/*MIDDLE CHARTS*/}
-                    <div className="flex gap-4 flex-col lg:flex-row">
-                        {/*COUNT CHART*/}
-                        <div className="w-full lg:w-1/3 h-[450px]">
-                            <CountChart/>
-                        </div>
-                        {/*ATTENDANCE CHART*/}
-                        <div className="w-full lg:w-2/3 h-[450px]">
-                            <AttendanceChart/>
-                        </div>
+                {/*MIDDLE CHARTS*/}
+                <div className="flex gap-4 flex-col lg:flex-row">
+                    {/*COUNT CHART*/}
+                    <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart/>
                     </div>
-                    {/*BOTTOM CHARTS*/}
+                    {/*ATTENDANCE CHART*/}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart/>
+                    </div>
+                </div>
+                {/*BOTTOM CHARTS*/}
+                <div className="w-full h-[500px]">
+                    <FinanceChart/>
                 </div>
             </div>
             {/*RIGHT SIDE*/}
