46f540d872b1c833aaa6f4145b86805cc2ff2cdb
lanangksma <kusumalanang1@gmail.com>
2024-09-17 00:36:26 +0700

    finish Parent dashboard page

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -9,7 +9,7 @@ const TeacherPage = () => {
             {/* LEFT */}
             <div className="w-full xl:w-2/3">
                 <div className="h-full bg-white p-4 rounded-md">
-                    <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+                    <h1 className="text-xl font-semibold">Schedule</h1>
                     <BigCalendar/>
                 </div>
             </div>
